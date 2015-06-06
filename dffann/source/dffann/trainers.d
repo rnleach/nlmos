e7a581136babc4715303e4fc018f6aed90f42a30
rnleach <ryan.n.leach@gmail.com>
2015-06-05 21:09:49 -0600

    Starting to make a module with trainers.

---
+++ dffann/source/dffann/trainers.d
@@ -0,0 +1,15 @@
+/**
+ * Author: Ryan Leach
+ * Version: 1.0.0
+ * Date: May 11, 2015
+ *
+ * This module contains classes for training networks.
+ *
+ */
+module dffann.trainers;
+
+import numeric.func;
+
+public import dffann.dffann;
+import dffann.data;
+import dffann.feedforwardnetwork;
